d84517df80a2309fb714c114ecc5dd92bcf46670
losing my sanity <trufanovila59@gmail.com>
2024-03-01 10:14:05 +0300

    1

--- task1/1.py
+++ task1/1.py
@@ -19,8 +19,27 @@ def findLongest(nums):
 
     return longest
 
+def longest_nondecreasing_subsequence(lst):
+    max_length = 0
+    max_sum = float('-inf')
+    max_sequence = []
+
+    for i in range(len(lst)):
+        for j in range(i, len(lst)):
+            if all(lst[k] <= lst[k+1] for k in range(i, j)):
+                current_length = j - i + 1
+                current_sum = sum(lst[i:j+1])
+                if current_length > max_length or (current_length == max_length and current_sum > max_sum):
+                    max_length = current_length
+                    max_sum = current_sum
+                    max_sequence = lst[i:j+1]
+    return max_sequence
+    
+
+
 with open('input.txt', 'r') as file:
     nums = [int(symbol) for symbol in list(file.readline().strip())]
 
 print(len(nums))
 print(findLongest(nums))
+print(longest_nondecreasing_subsequence(nums))
